630b1c41844d8d2f77a77546af55559e7e0b5add
jztlucky6264 <77235457+jztlucky6264@users.noreply.github.com>
2022-05-30 22:22:01 +0530

    make dynamic  set commit

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const api = {
   key: "dc62d4546785d5c9f59a6ec5f3682b5a",
-
   base: "https://api.openweathermap.org/data/2.5/",
 };
 const searchinputbox = document.querySelector(".input-box");
-
-searchinputbox.addEventListener("keypress", setQuery);
+if (searchinputbox.value == "") {
+  getWeatherReport("delhi");
+}
+searchinputbox.addEventListener("keyup", setQuery);
 
 function setQuery(event) {
-  if (event.keyCode == 13) {
+  if (event) {
     console.log(searchinputbox.value);
     getWeatherReport(searchinputbox.value);
   }
@@ -41,9 +42,24 @@ function showWeatherReport(weather) {
   let date = document.querySelector(".date");
   date.innerText = dateupdate(now);
 
-  if (WeatherType.textContent == "Smoke") {
+  if (WeatherType.textContent == "") {
     document.body.style.backgroundImage =
-      "url('C:/Users/lucky yadav/ Pictures/guitar.jpg')";
+      "url('https://images.unsplash.com/photo-1500534623283-312aade485b7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80')";
+  } else if (WeatherType.textContent == "Rain") {
+    document.body.style.backgroundImage =
+      "url('https://images.unsplash.com/photo-1534274988757-a28bf1a57c17?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80')";
+  } else if (WeatherType.textContent == "Haze") {
+    document.body.style.backgroundImage =
+      "url('https://images.unsplash.com/36/STzPBJUsSza3mzUxiplj_DSC09775.JPG?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=742&q=80')";
+  } else if (WeatherType.textContent == "Mist") {
+    document.body.style.backgroundImage =
+      "url('https://images.unsplash.com/photo-1485236715568-ddc5ee6ca227?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80')";
+  } else if (WeatherType.textContent == "Clear") {
+    document.body.style.backgroundImage =
+      "url('https://images.unsplash.com/photo-1517574150334-6d3578f2cc5a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80')";
+  } else if (WeatherType.textContent == "Clouds") {
+    document.body.style.backgroundImage =
+      "url('https://images.unsplash.com/photo-1594156596782-656c93e4d504?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80')";
   }
 }
 
